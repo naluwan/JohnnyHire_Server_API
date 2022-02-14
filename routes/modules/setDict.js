7503eaa27999d0e7208f543aed90b698a940ad45
sofa566 <sofa566@gmail.com>
2022-02-14 10:35:05 +0800

    feat: 新增徵厲害公司資訊公司資訊dict router

--- routes/modules/setDict.js
+++ routes/modules/setDict.js
@@ -14,4 +14,15 @@ router.post('/jh/position', (req, res) => {
   res.status(200).send(dict)
 })
 
+// 徵厲害新增公司資訊dict
+router.post('/jh/cpnyInfo', (req, res) => {
+  const {cpnyInfo_name} = req.query
+  const text = `${cpnyInfo_name} 20\n`
+
+  fs.appendFileSync(path.resolve(__dirname, '/home/bill/桌面/Work/BF25_JohnnyHire/dict/cpnyInfo.txt'), text, 'utf-8', '0o666', 'as+')
+  const dict = fs.readFileSync(path.resolve(__dirname, '/home/bill/桌面/Work/BF25_JohnnyHire/dict/cpnyInfo.txt'), 'utf-8')
+
+  res.status(200).send(dict)
+})
+
 module.exports = router
